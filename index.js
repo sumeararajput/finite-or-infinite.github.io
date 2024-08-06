67e1429b905f5256380020f94a5262b29ce26836
sumeararajput <sumeara123.bmj@gmail.com>
2024-08-06 10:41:09 -0700

    finite-or-infinite 6aug

---
+++ index.js
@@ -0,0 +1,14 @@
+let num1 = parseInt(prompt("write anynumber do you want to divide"));
+let num2 = parseInt(prompt("please Enter last num"));
+
+let result = num1/num2;
+if(Number.isFinite(result)){
+ console.log(result);
+}
+else{
+  console.log("math error");
+}
+
+
+
+
